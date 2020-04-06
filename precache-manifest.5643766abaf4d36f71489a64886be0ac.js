369f17530c6c4c9ec139083b925862064bfc2f78
abdiqayum <isacad1920@gmail.com>
2020-04-07 00:03:46 +0300

    Updates

---
+++ precache-manifest.5643766abaf4d36f71489a64886be0ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1186487a553d9e9b6c4ada86c7414d5b",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "bf7c3ef963c8e9cc600e",
+    "url": "/covid19/static/css/main.9836ce93.chunk.css"
+  },
+  {
+    "revision": "6912ae51a3c734005ad2",
+    "url": "/covid19/static/js/2.5251b60e.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/covid19/static/js/2.5251b60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7c3ef963c8e9cc600e",
+    "url": "/covid19/static/js/main.32ea028c.chunk.js"
+  },
+  {
+    "revision": "33db6a2cd22e70a8b390",
+    "url": "/covid19/static/js/runtime-main.6e11f889.js"
+  },
+  {
+    "revision": "34967c3bdacffdb6bb6882b73d62c3eb",
+    "url": "/covid19/static/media/coronavirus.34967c3b.png"
+  }
+]);
